90faa2de0c023775be5bb0c466b31a08038a484b
Adhiban Siddarth V <siddhu1131@gmail.com>
2022-02-21 12:09:15 +0530

    change

--- RoboArm/Graph_animation/RoboArmAnim.py
+++ RoboArm/Graph_animation/RoboArmAnim.py
@@ -3,55 +3,70 @@ import numpy as np
 
 fig, ax = plt.subplots()
 
-def arm(x,y):
-    target = (x,y)
-    ax.set_xlim(-3,3)
-    ax.set_ylim(-3,3)
-    distance = np.sqrt(target[0]**2+target[1]**2)
+a = np.linspace(0, np.pi / 2, 100)
+c = 2 * np.cos(a)
+s = 2 * np.sin(a)
+
+
+def arm(x, y):
+    target = (x, y)
+    ax.set_xlim(-3, 3)
+    ax.set_ylim(-3, 3)
+    distance = np.sqrt(target[0] ** 2 + target[1] ** 2)
     if distance < 2 and x > 0 and y > 0:
-        a1 = np.arctan(target[1]/target[0]) + \
-            np.arctan(np.sqrt((4-target[0]**2-target[1]**2)/(target[0]**2+target[1]**2)))
+        a1 = np.arctan(target[1] / target[0]) + np.arctan(
+            np.sqrt(
+                (4 - target[0] ** 2 - target[1] ** 2)
+                / (target[0] ** 2 + target[1] ** 2)
+            )
+        )
 
         x = [0, np.cos(a1), target[0]]
         y = [0, np.sin(a1), target[1]]
 
-        ax.plot(x,y, '-o')
+        ax.plot(x, y, "-o")
         # ax.grid(True)
-        ax.plot([-3,3],[0,0], '--')
+        ax.plot([-3, 3], [0, 0], "--")
+        ax.plot(c, s, "r")
+        ax.plot([0, 0], [0, 2], "r")
         # plt.show()
     else:
-        print('********** Out of Range **********')
+        print("********** Out of Range **********")
 
 
 def move(p1, p2):
-    
-    x = [p1[0],p2[0]]
-    y = [p1[1],p2[1]]
 
-    if (x[1]-x[0]) != 0:
+    x = [p1[0], p2[0]]
+    y = [p1[1], p2[1]]
+
+    if (x[1] - x[0]) != 0:
         l = np.linspace(x[0], x[1], 20)
-        m = (l-x[0])*(y[1]-y[0])/(x[1]-x[0])+y[0]
-        
-        for i,j in zip(l,m):
+        m = (l - x[0]) * (y[1] - y[0]) / (x[1] - x[0]) + y[0]
+
+        for i, j in zip(l, m):
             ax.cla()
-            ax.scatter(p2[0],p2[1], c='green')
-            arm(i,j)
+            ax.scatter(p2[0], p2[1], c="green")
+            arm(i, j)
             # ax.set_title("frame {}".format(i)
             # Note that using time.sleep does *not* work here!
             plt.pause(0.01)
     else:
-        print('***********  Invalid  **********')
+        print("***********  Invalid  **********")
 
-def main(co_ord):
-    for i in co_ord:
+
+def main(movepath):
+    for i in movepath:
         move(i[0], i[1])
 
-co_ord = [
-    [(0.01,1),(1.9,0.1)],
-    [(1.9,0.1),(np.sqrt(1.9),np.sqrt(1.9))],
-    [(np.sqrt(1.9),np.sqrt(1.9)),(0.01,1)]
-]
+
+points = [(0.01, 1), (1.9, 0.1), (np.sqrt(1.9), np.sqrt(1.9)), (0.01, 1)]
+
+movepath = []
+
+for i in range(len(points) - 1):
+    movepath.append([points[i], points[i + 1]])
 
 while True:
-    main(co_ord)
+    main(movepath)
+# arm(1, 1)
 # plt.show()
